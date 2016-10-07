d0e6dc43e765d1d0b7c0809ce9cdf4735f3048f0
Ivan <wcy5211678@163.com>
2016-10-07 19:33:24 +0800

    更新模板

--- template/webpack.config.js
+++ template/webpack.config.js
@@ -33,6 +33,14 @@ module.exports = {
         query: {
           name: '[name].[ext]?[hash]'
         }
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "url-loader?limit=10000&minetype=application/font-woff"
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "file-loader"
       }
     ]
   },
