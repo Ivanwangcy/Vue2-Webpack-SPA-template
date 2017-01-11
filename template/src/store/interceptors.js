2a8e3b3645462614cbb2c01272175a9a02a63633
Ivan <wcy5211678@163.com>
2017-01-11 12:02:35 +0800

    add interceptors

--- template/src/store/interceptors.js
+++ template/src/store/interceptors.js
@@ -8,7 +8,7 @@ export default (store) => {
 
   // JSON对象参数化
   Vue.http.options.emulateJSON = true
-  
+
   // 自定义请求头
   Vue.http.headers.common['token'] = '' + Date.now()
 
@@ -16,7 +16,8 @@ export default (store) => {
     // 请求前的处理逻辑
     //
     // 增加公共参数
-    request.params = {...request.params, platCode: 'H5', appName: 'app', appVersion: '1.0'}
+    request.params.body = JSON.stringify(request.params.body);
+    request.params = {...request.params, platCode: 'H5', appName: 'paidaojia', appVersion: '3.8.0'}
 
     store.commit('loading', true) // loading true
     next((response) => {
